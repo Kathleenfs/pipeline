58b778e6e8cd7f1be3bf8ef2dbdbcdb54001592f
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2022-09-06 20:03:30 -0300

    enviando java para pipeline

---
+++ src/main/java/com/teste/rodando/RodandoApplication.java
@@ -0,0 +1,13 @@
+package com.teste.rodando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodandoApplication.class, args);
+	}
+
+}
